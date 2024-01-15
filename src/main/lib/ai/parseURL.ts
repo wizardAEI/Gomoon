db174665e449551bfee11650b2e48e18e0336c21
wizardAEI <imaei@foxmail.com>
2024-01-15 19:33:23 +0800

    feat: 交互优化

--- src/main/lib/ai/parseURL.ts
+++ src/main/lib/ai/parseURL.ts
@@ -1,6 +1,35 @@
 import puppeteer from 'puppeteer'
+import fetch from 'node-fetch'
+import { Cheerio, Element, load } from 'cheerio'
 // TODO: 转md
 export async function parseURL2Str(url: string) {
+  const html = await fetch(url).then((res) => res.text())
+  const $ = load(html)
+  $('script').remove()
+  const title = $('title').text()
+  // 去除回车和空格
+  const bodyContent = $('body').text().replace(/\s+/g, '')
+  if (bodyContent.length > 50) {
+    function mainContent(node: Cheerio<Element>) {
+      for (let i = 0; i < node.contents().length; i++) {
+        const child = node.contents()[i]
+        if (child.type === 'text') {
+          // 如果是文本节点，则替换文本
+          return $(child).text()
+        } else if (
+          child.type === 'tag' &&
+          !['script', 'style', 'svg'].includes(child.tagName.toLowerCase()) &&
+          $(child).text().length > $('body').text().length * 0.8
+        ) {
+          return mainContent($(child))
+        }
+      }
+      return node.text()
+    }
+    return `${title}\n\n` + mainContent($('body'))
+  }
+
+  // 如果内容过少，则怀疑为单页面应用或遇到安全验证，使用puppeteer解析
   const page = await (await puppeteer.launch()).newPage()
   try {
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 })
@@ -9,7 +38,8 @@ export async function parseURL2Str(url: string) {
     return '内容解析失败'
   }
   const mainContent = await page.evaluate(() => {
-    const total = document.querySelector('body')
+    const title = document.querySelector('title')?.innerText || ''
+    const total = document.querySelector('body') || ''
     if (!total) return '...'
     const totalLength = total.innerText.length
     const parseNode = (
@@ -28,7 +58,9 @@ export async function parseURL2Str(url: string) {
           el.nodeName === 'SCRIPT' ||
           el.nodeName === 'STYLE' ||
           el.nodeName === 'NOSCRIPT' ||
-          el.nodeName === 'IFRAME'
+          el.nodeName === 'IFRAME' ||
+          el.nodeName === 'LINK' ||
+          el.nodeName === 'SVG'
         )
           // 排除一些不需要的元素，例如script
           return {
@@ -54,7 +86,6 @@ export async function parseURL2Str(url: string) {
       return ele.innerText
     }
     // 获取标题
-    const title = document.querySelector('title')?.innerText || ''
     return `${title}\n\n` + findMainContent(total)
   })
   // 释放资源
