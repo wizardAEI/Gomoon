82511bb99023185ddb8e6e9535cc04695547a7c1
wizardAEI <imaei@foxmail.com>
2024-05-27 19:43:02 +0800

    feat: 1.1.2

--- src/renderer/src/pages/Setting/VersionDesc.tsx
+++ src/renderer/src/pages/Setting/VersionDesc.tsx
@@ -5,6 +5,10 @@ import { For, Show, createSignal } from 'solid-js'
 
 const versions = [
   {
+    version: '1.1.2',
+    contents: ['修复一些消息历史模块的已知问题。', '消息历史页面样式优化。']
+  },
+  {
     version: '1.1.1',
     contents: [
       '历史消息功能升级，支持了更多实用功能！',
